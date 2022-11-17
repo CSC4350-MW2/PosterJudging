cbe4271001ab6cbcc29c688982ac5938ccf1417e
Sabr Suleiman <invictus@Sabrs-Air.wifi.gsu.edu>
2022-11-17 15:05:38 -0500

    fixed template dir

--- website/views.py
+++ website/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     if request.method == 'GET':
-        return render(request, "templates/home.html")
+        return render(request, "home.html")
     elif request.method == 'POST':
         return HttpResponse("Thanks for post request!")
