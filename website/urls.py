33e802ea64b5e2983fc061e1a7714a713e53f411
sol <sbrsuleiman@gmail.com>
2022-11-24 17:22:48 -0500

    added dynamic routing and judge login

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('administrator', views.admin_index, name='administrator'),
+    path('<str:session_id>/judge-login', views.judge_login, name='judge-login'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import session
 
 def index(request):
@@ -8,8 +8,19 @@ def index(request):
     elif request.method == 'POST':
         post = session()
         post.id = request.POST['id']
-        return HttpResponse(post.id)
+        post.save()
+
+        context = {
+            "session_id": post.id
+        }
+
+        request.session['session_id'] = post.id
+        return redirect(post.id+'/judge-login')
 
 def admin_index(request):
     if request.method == 'GET':
         return render(request, "website/administrator.html")
+
+def judge_login(request, session_id):
+    Session = session.objects.get(id=session_id)
+    return render(request, "website/judge_login.html")
