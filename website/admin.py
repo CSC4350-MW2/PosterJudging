38b4bbc9db2a9e57444c7eddad0c863fa69d1ca4
sol <sbrsuleiman@gmail.com>
2022-11-25 00:34:27 -0500

    fixed admin models

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import session
+from .models import session, judge, administrator, submission
 
 admin.site.register(session)
+admin.site.register(judge)
+admin.site.register(administrator)
+admin.site.register(submission)
--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import session
+from .models import session, judge
 
 def index(request):
     if request.method == 'GET':
-        return render(request, "website/home.html")
+        return render(request, 'website/home.html')
     elif request.method == 'POST':
         post = session()
         post.id = request.POST['id']
@@ -19,8 +19,18 @@ def index(request):
 
 def admin_index(request):
     if request.method == 'GET':
-        return render(request, "website/administrator.html")
+        return render(request, 'website/administrator.html')
 
 def judge_login(request, session_id):
-    Session = session.objects.get(id=session_id)
-    return render(request, "website/judge_login.html")
+    if request.method == 'GET':
+        Session = session.objects.get(id=session_id)
+        return render(request, 'website/judge_login.html')
+    elif request.method == 'POST':
+        post = judge()
+        post.first_name = request.POST['first_name']
+        post.last_name = request.POST.get('last_name', '')
+        post.panther_id = request.POST['panther_id']
+        post.subject = request.POST['subject_choices']
+        post.level = request.POST['level_choices']
+        post.save()
+        return HttpResponse("Logged In :)")
