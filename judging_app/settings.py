50d2a9e5227ce26d915b80a2bde4ec4a21a20752
Ngo <ngo@linhngo.local>
2022-11-25 05:34:55 -0500

    created register

--- judging_app/settings.py
+++ judging_app/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms",
+    "register.apps.RegisterConfig",
+
 ]
 
 MIDDLEWARE = [
@@ -123,6 +126,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = "/administrator"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- judging_app/urls.py
+++ judging_app/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls'))
+    path("register/", v.register, name="register"),
+    path('', include('website.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
