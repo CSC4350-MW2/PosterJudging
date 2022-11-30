78d81584fb15f0cf2c8634f8fa5e76e3ff39506c
sol <sbrsuleiman@gmail.com>
2022-11-29 19:47:11 -0500

    changed administrator to user

--- register/views.py
+++ register/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm, LoginForm
 from .models import administrator
+from django.contrib.auth.models import User, Group 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 
 def register(response):
+    # In future, check if passwords match
     if response.method == "POST":
         form = RegisterForm(response.POST)
-        post = administrator()
-        post.username = response.POST['username']
-        post.email = response.POST['email']
-        post.password = response.POST['password1']
-        post.save()
+        username = response.POST['username']
+        email = response.POST['email']
+        password = response.POST['password1']
+        user = User.objects.create_user(username, email, password)
+        user.save()
 
         return redirect("login")
     else:
@@ -21,7 +25,9 @@ def register(response):
 
 def login(response):
     if response.method == "POST":
-        return redirect("administrator/")
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
     else:
         form = LoginForm()
         return render(response, "registration/login.html", {"form": form})
